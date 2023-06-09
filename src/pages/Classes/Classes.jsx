ff6833ea05d529602d055648cf69b74a561bedab
a-ashik <asikahmeed7@gmail.com>
2023-06-09 10:56:39 +0600

    classes added

--- src/pages/Classes/Classes.jsx
+++ src/pages/Classes/Classes.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import SingleClass from '../SingleClass/SingleClass';
 
 const Classes = () => {
+
+     const [classData,setClassData] = useState([])
+
+
+    useEffect(() => {
+        fetch('http://localhost:5000/classes')
+        .then((res) => res.json())
+        .then((data) =>setClassData(data))
+    },[])
+
+    console.log(classData);
+
     return (
         <div>
-            <h1>i am classes</h1>
+            {
+                classData.map((data) =>
+                <SingleClass
+                 key={data._id}
+                 data={data}
+                ></SingleClass>
+                )
+            }
         </div>
     );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Slider from '../Slider/Slider';
 
 const Home = () => {
+
+    
+
     return (
         <div className=''>
             <Slider></Slider>
